2ce89cb5796ca11fb72a3a318f7393aeb873a993
anactasi2607 <anactasi2607@yandex.ru>
2019-11-14 01:02:59 +0300

    Добавляет ограничение по форматам файлов, добавляет возможность загрузить несколько фото в объявление, меняет функцию по очистке полей с фото

--- js/upload-photo.js
+++ js/upload-photo.js
@@ -6,14 +6,23 @@
   var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
   var PHOTO_PREVIEW_WIDTH = 70;
   var PHOTO_PREVIEW_HEIGHT = 70;
+  var PHOTO_NUMBER = 10;
+  var PHOTO_MARGIN = 3;
 
   var avatarFileChooser = document.querySelector('#avatar');
   var avatarPreview = document.querySelector('.ad-form-header__preview img');
-
   var photoFileChooser = document.querySelector('#images');
   var photoPreview = document.querySelector('.ad-form__photo');
 
-  function getImagePreview(fileChooser, preview) {
+  function setAcceptAttribute(elem) {
+    elem.setAttribute('accept', 'image/png, image/gif, image/jpg, image/jpeg');
+  }
+
+  setAcceptAttribute(photoFileChooser);
+  setAcceptAttribute(avatarFileChooser);
+  photoFileChooser.setAttribute('multiple', 'true');
+
+  function getOneImagePreview(fileChooser, preview) {
     var file = fileChooser.files[0];
     var fileName = file.name.toLowerCase();
 
@@ -35,26 +44,31 @@
   }
 
   avatarFileChooser.addEventListener('change', function () {
-    getImagePreview(avatarFileChooser, avatarPreview);
+    getOneImagePreview(avatarFileChooser, avatarPreview);
   });
 
   photoFileChooser.addEventListener('change', function () {
-    var housePhotoImg = photoPreview.querySelector('img');
+    var housePhotoImg = photoPreview.querySelectorAll('img');
+    var photoArray = Array.from(housePhotoImg);
 
-    if (!housePhotoImg) {
+    if (photoArray.length < PHOTO_NUMBER) {
       housePhotoImg = document.createElement('img');
       housePhotoImg.style.width = PHOTO_PREVIEW_WIDTH + 'px';
       housePhotoImg.style.height = PHOTO_PREVIEW_HEIGHT + 'px';
+      housePhotoImg.style.margin = PHOTO_MARGIN + 'px';
       photoPreview.appendChild(housePhotoImg);
+      getOneImagePreview(photoFileChooser, housePhotoImg);
     }
-
-    getImagePreview(photoFileChooser, housePhotoImg);
   });
 
   function deleteFormImage() {
-    var housePhotoImg = photoPreview.querySelector('img');
-    if (housePhotoImg) {
-      housePhotoImg.remove();
+    var housePhotoImg = photoPreview.querySelectorAll('img');
+    var photoArray = Array.from(housePhotoImg);
+
+    if (photoArray) {
+      photoArray.forEach(function (item) {
+        item.remove();
+      });
     }
     avatarPreview.src = 'img/avatars/default.png';
   }
